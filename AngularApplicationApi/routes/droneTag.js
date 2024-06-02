7043e33564f436894bcb5e2d8e95d2aa1ba6376f
jubalmurrian <JUBAL.MURRIAN@knights.ucf.edu>
2024-06-02 23:49:21 +0000

    AngularApplicationApi

--- AngularApplicationApi/routes/droneTag.js
+++ AngularApplicationApi/routes/droneTag.js
@@ -3,12 +3,14 @@ const db = require('../db/db');
 const router = require("express").Router();
 
 router.get('/positionreport/:id',async (req,res,next)=>{
-    let positionreport = await db.get().positionreport.findByIds([req.params['id']]);
+    const database = await db.get();
+    let positionreport = await database.positionreport.findByIds([req.params['id']]).exec();
     res.send(positionreport);
 });
 
 router.post('/positionreport',async (req,res,next)=>{
-    await db.get().patch(req.body);
+    const database = await db.get();
+    await database.patch(req.body);
 });
 
 module.exports = router;
